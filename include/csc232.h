b14df90632cd75040d600f6c2c6757d7cef6bd20
Sam <samsaddress964@gmail.com>
2024-01-30 17:13:29 -0600

    completed assignment

--- include/csc232.h
+++ include/csc232.h
@@ -18,9 +18,9 @@
 #define TRUE 1
 #define EXECUTE_BLOCK FALSE
 
-#define FINISHED_PART_1 FALSE
-#define FINISHED_PART_2 FALSE
-#define FINISHED_PART_3 FALSE
+#define FINISHED_PART_1 TRUE
+#define FINISHED_PART_2 TRUE
+#define FINISHED_PART_3 TRUE 
 
 #include <algorithm>
 #include <cassert>
@@ -60,14 +60,56 @@ using std::setw;
  */
 namespace csc232
 {
-    // Add any user-defined functions prescribed in your assignment below
-    // TODO: 1.1 Declare the Shape interface below
+        // Add any user-defined functions prescribed in your assignment below
+        // TODO: 1.1 Declare the Shape interface below
+        class Shape
+        {
+                public:
+                        virtual double area() const = 0;
+                        virtual double perimeter() const = 0; 
+                        virtual ~Shape() = default;
+        };
 
-    // TODO: 2.1 Provide an inline definition of the Square class below
+        // TODO: 2.1 Provide an inline definition of the Square class below
+        class Square : public Shape
+        {
+                public:
+                        double area() const override
+                        {
+                                return side * side;
+                        }
 
-    // TODO: 3.1 Provide an inline definition of the Circle class below
+                        double perimeter() const override
+                        {
+                                return side * 4;
+                        } 
 
-    // DO NOT Modify anything below this line
+                        ~Square() = default;
+
+                private:
+                        double side = 1;
+        };
+
+        // TODO: 3.1 Provide an inline definition of the Circle class below
+        class Circle : public Shape
+        {
+                public:
+                        double rad = 1;
+
+                        double area() const override
+                        {
+                                return M_PI * (rad * rad);
+                        }
+
+                        double perimeter() const override
+                        {
+                                return 2 * M_PI * rad;
+                        } 
+
+                        ~Circle() = default;
+        };
+
+        // DO NOT Modify anything below this line
 } // namespace csc232
 
 #endif // CSC232_COMMON_H_
